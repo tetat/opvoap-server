56675931bcfb9958f3e791b46d75c5bf6aeecc9b
tetat <nishat00101@gmail.com>
2023-09-09 12:16:08 +0600

    test change

--- controller/userController.js
+++ controller/userController.js
@@ -99,18 +99,22 @@ module.exports.currentUser = async (req, res) => {
   // console.log("jwt: ", req.cookies.jwt);
   try {
     const id = tokenId(req.cookies.jwt);
-    // console.log("id: ", id);
-    const people = await Peoples.findById({ _id: id }).select({
-      _id: 0,
-      __v: 0,
-      password: 0,
+
+    res.status(500).json({
+      id: id,
+      jwt: req.cookies.jwt,
     });
-    // console.log("id: ", id);
-    if (people) {
-      res.status(201).json(people);
-    } else {
-      res.status(400).json({ id: "id not valid" });
-    }
+    // const people = await Peoples.findById({ _id: id }).select({
+    //   _id: 0,
+    //   __v: 0,
+    //   password: 0,
+    // });
+    // // console.log("id: ", id);
+    // if (people) {
+    //   res.status(201).json(people);
+    // } else {
+    //   res.status(400).json({ id: "id not valid" });
+    // }
   } catch (err) {
     // console.log("error: ", err);
     res.status(500).send({ err });
